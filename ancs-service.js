ba9e166801d82355699ca8f0c1639d12563b3e9d
MudkipTheEpic <MudkipTheEpic@users.noreply.github.com>
2017-06-10 01:20:11 -0700

    Add generic characteristic to service

--- ancs-service.js
+++ ancs-service.js
@@ -2,6 +2,7 @@ var util = require('util');
 
 var able = require('./index');
 var AblePrimaryService = able.PrimaryService;
+var GenericCharacteristic = require('./generic-characteristic')
 
 //var Blink1RGBCharacteristic = require('./blink1-rgb-characteristic');
 //var Blink1FadeRGBCharacteristic = require('./blink1-fade-rgb-characteristic');
@@ -9,7 +10,9 @@ var AblePrimaryService = able.PrimaryService;
 function AncsService() {
   AncsService.super_.call(this, {
     uuid: '7905f431b5ce4e99a40f4b1e122d00d0',
-    characteristics: []
+    characteristics: [
+      new GenericCharacteristic()
+    ]
   });
 }
 
